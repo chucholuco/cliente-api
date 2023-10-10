c69802bd40b4718074a7538014d4263bf6323b9b
Jesus Tapia <chucholuco@gmail.com>
2023-10-10 11:45:03 -0600

    adding cliente and nuevo cliente

--- src/components/clientes/Clientes.js
+++ src/components/clientes/Clientes.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState, Fragment } from 'react'
+import clienteAxios from '../../config/axios'
+import Cliente from './Cliente'
+import { Link } from 'react-router-dom'
 
-const Clientes = () => {    
+const Clientes = () => {  
+    
+    // Trabajar con el state
+    const [clientes, guardarClientes] = useState([])
+
+    // Query a la API
+    const consultarAPI = async () => {
+        const clientesConsulta = await clienteAxios.get('/clientes')
+        guardarClientes(clientesConsulta.data)
+    }
+
+    useEffect(() => {
+        consultarAPI()
+    }, [])
+
     return (
-        <h2>Clientes</h2>
+        <Fragment>
+            <h2>Clientes</h2>
+            <Link to={"/clientes/nuevo"} className="btn btn-verde nvo-cliente"> <i className="fas fa-plus-circle"></i>
+                Nuevo Cliente
+            </Link>
+            <ul className='listado-clientes'>
+                {clientes.map(cliente => (
+                    <Cliente 
+                        key={cliente._id}
+                        cliente={cliente}                        
+                    />
+                ))}
+            </ul>
+        </Fragment>
     )
 }
 
